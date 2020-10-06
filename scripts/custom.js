dae037e5bbcaef478d8f9c97ab393dff7be6e816
Beau324 <Beau.cunningham324@gmail.com>
2020-10-06 12:28:26 -0500

    Initial commit

---
+++ scripts/custom.js
@@ -0,0 +1,39 @@
+﻿$(function () {
+
+    $('#contact_form').validator();
+
+    $('#contact_form').on('submit', function (e) {
+        if (!e.isDefaultPrevented()) {
+            var url = "contact_form/contact_form.php";
+
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(this).serialize(),
+                success: function (data) {
+                    var messageAlert = 'alert-' + data.type;
+                    var messageText = data.message;
+
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    if (messageAlert && messageText) {
+                        $('#contact_form').find('.messages').html(alertBox);
+                        $('#contact_form')[0].reset();
+                    }
+                }
+            });
+            return false;
+        }
+    });
+});
+function palindrome() {
+    let userStr = document.getElementById("str").value;
+    let rev = userStr.split('').reverse().join('');
+    if (userStr == rev) {
+        let resultp = userStr + " is a palindrome";
+        document.getElementById("resultp").innerHTML = `<b>${resultp}</b>`;
+    }
+    else {
+        let resultp = userStr + " is not a palindrome";
+        document.getElementById("resultp").innerHTML = `<b>${resultp}</b>`;
+    }
+}
